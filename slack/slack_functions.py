7a77d297c82162ba83c6d599c54818a91f1f67f3
Hunter Sinclair <hsinclair@thinktiv.com>
2023-09-15 23:48:08 -0500

    new version

--- slack/slack_functions.py
+++ slack/slack_functions.py
@@ -12,6 +12,7 @@ from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.callbacks.base import BaseCallbackHandler
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
 import logging
 
 # requires importing logging
@@ -216,32 +217,49 @@ def slack_respond_to_gpt_conversation(agent, ack, app, say, body):
     # Write message log to Supabase
     write_message_log(user_name=user_diplay_name, message=msg)
     
+    
+    my_messages = [
+    	SystemMessage(content=f"You are large language model chatbot assistant at {demo_company_name} trained by OpenAI. Your name is {ai_name}. You answer questions about anything, as factually as you can based on the information you know, and will attempt to provide references to where your answers came from."),
+    	HumanMessage(content=f"My name is {user_real_name}"),
+    	HumanMessage(content=msg)
+	]
+    results = llm(my_messages)
+    logging.debug('llm results: %s', results)
+    
     # Prompt
-    prompt=ChatPromptTemplate(input_variables=["question"],
-		messages=[
-			SystemMessagePromptTemplate.from_template(
-				f"You are large language model chatbot assistant at {demo_company_name} trained by OpenAI. Your name is {ai_name}. You answer questions about anything, as factually as you can based on the information you know, and will attempt to provide references to where your answers came from."
-			),
-			HumanMessagePromptTemplate.from_template(f"My name is {user_real_name}"),
-			MessagesPlaceholder(variable_name=f"chat_history_{user_id}"),
-			HumanMessagePromptTemplate.from_template("{question}")
-		]
+    chat_id = f"chat_history_{user_id}"
+    prompt=ChatPromptTemplate.from_messages([
+		SystemMessagePromptTemplate.from_template(
+			"You are large language model chatbot assistant at {demo_company_name} trained by OpenAI. Your name is {ai_name}. You answer questions about anything, as factually as you can based on the information you know, and will attempt to provide references to where your answers came from."
+		),
+		HumanMessagePromptTemplate.from_template("My name is {user_real_name}"),
+		HumanMessagePromptTemplate.from_template("{question}")
+	])
+	# MessagesPlaceholder(variable_name=chat_id),
+
+    messages = prompt.format_messages(
+		demo_company_name=demo_company_name,
+		ai_name=ai_name,
+		user_real_name=user_real_name,
+		question=msg
 	)
+    logging.debug('messages: %s', messages)
 
 	# Notice that we `return_messages=True` to fit into the MessagesPlaceholder
 	# Notice that `"chat_history"` aligns with the MessagesPlaceholder name
-    memory = ConversationBufferMemory(memory_key=f"chat_history_{user_id}",return_messages=True)
+    # memory = ConversationBufferMemory(memory_key=chat_id,return_messages=True)
     conversation = LLMChain(
 		llm=llm,
 		prompt=prompt,
 		verbose=True,
-		memory=memory
+		memory=ConversationBufferMemory()
 	)
-
+	#memory=memory
+	
 	# Notice that we just pass in the `question` variables - `chat_history` gets populated by memory
     response = conversation.run({"question": msg})
-	
-    logging.debug(f"RESPONSE **** : {response}")
+
+    logging.debug('RESPONSE ****: %s', response)
 	
     blocks = [
         {
